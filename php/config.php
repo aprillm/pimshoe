5da21b6c619d54742471c323a2d5807278e6ad4b
aprillm <60377076+aprillm@users.noreply.github.com>
2020-02-19 14:59:05 -0500

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,9 +1,9 @@
 <?php
 	/*DB credentials should be inserted here. */
-	$servername = 'servername';
-	$username   = 'username';
-	$password   = 'password';
-	$dbname     = 'dbname';<br> 
+	$servername = "52.204.100.89";
+	$username   = "administrator";
+	$password   = "youdiedpimshoe";
+	$dbname     = "cpsc445-capstone.cah4eqmlcf2h.us-east-1.rds.amazonaws.com";<br> 
 	$conn = mysqli_connect($servername, $username, $password);
 	
 	$dbcon = mysqli_select_db($conn,$dbname);
