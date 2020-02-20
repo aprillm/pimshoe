8dd907cd8490fa620d78fb119d495170dc35382d
aprillm <60377076+aprillm@users.noreply.github.com>
2020-02-20 11:54:06 -0500

    Update create.php

--- php/admin/create.php
+++ php/admin/create.php
@@ -39,8 +39,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         </a>
         </div>
         <div class="col-sm-4">
-            <a href="#" class="btn btn-primary btn-block btn-lg p-5">
-          Create Employee
+            <a href="createuser.php" class="btn btn-primary btn-block btn-lg p-5">
+          Create Employee User
           </a>
           </div>
       </div>
