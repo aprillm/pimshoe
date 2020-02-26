9f614d4a806574dd4094fb3487424834b6eb7158
tylerhiu <60403530+tylerhiu@users.noreply.github.com>
2020-02-26 13:44:22 -0500

    Update config.php
    Updated config.php to display properly on web browser

--- php/config.php
+++ php/config.php
@@ -1,18 +1,18 @@
 <?php
-	/*DB credentials should be inserted here. */
-	$servername = "52.204.100.89";
-	$username   = "administrator";
-	$password   = "youdiedpimshoe";
-	$dbname     = "cpsc445-capstone.cah4eqmlcf2h.us-east-1.rds.amazonaws.com";
-	$conn = mysqli_connect($dbname, $username, $password);
-	
-	$dbcon = mysqli_select_db($conn,$dbname);
-	// Check connection
-	if (!$conn) {
-		die("Connection failed: " . mysqli_connect_error());
-	}
- 
-	if ( !$dbcon ) {
-		die("Database Connection failed : " . mysqli_error());
-	}
+        /*DB credentials should be inserted here. */
+        $servername = "cpsc445-capstone.cah4eqmlcf2h.us-east-1.rds.amazonaws.com";
+        $username   = "administrator";
+        $password   = "youdiedpimshoe";
+        $dbname     = "PIMSHOE";
+        $conn = mysqli_connect($servername, $username, $password);
+
+        $dbcon = mysqli_select_db($conn,$dbname);
+        // Check connection
+        if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+        }
+
+        if ( !$dbcon ) {
+                die("Database Connection failed : " . mysqli_error());
+        }
 ?>
