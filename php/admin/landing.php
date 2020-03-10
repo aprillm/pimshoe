47dedaf562c04c6f7f0c649b2316aa4503023e3a
aprillm <60377076+aprillm@users.noreply.github.com>
2020-03-10 18:03:06 -0400

    Update landing.php

--- php/admin/landing.php
+++ php/admin/landing.php
@@ -2,7 +2,7 @@
 
 //is user logged in?
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-	header("location: http://3.211.215.236/login.php");
+	header("location: http://52.204.100.89/login.php");
 	exit;
 }
 ?>
