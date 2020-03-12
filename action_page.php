555caf0668aadd9886362e0fb4e8022740ec5f7b
jckri <60411822+jckri@users.noreply.github.com>
2020-03-12 17:52:22 -0400

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "pwdpwd";
+$dbname = "pimshoe";
+
+
+// Create connection
+$mysqli = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($mysqli->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['uname'];
+$password = sha1($_POST['psw']);
+
+
+$query = mysqli_query($mysqli,"SELECT email FROM user WHERE email = '$username' AND passhash = '$password'");
+$total = mysqli_num_rows($query);
+
+if($total > 0)
+{
+  echo "Successful Login";
+}
+else {
+  echo "Unsuccessful Login";
+}
+
+?>
