cd159a18b01480546292406d79e0883abc7de33b
tylerhiu <60403530+tylerhiu@users.noreply.github.com>
2020-03-24 21:11:44 -0400

    Update landing.php

--- landing.php
+++ landing.php
@@ -18,7 +18,7 @@ $username = $_POST['uname'];
 $password = sha1($_POST['psw']);
 
 
-$query = mysqli_query($mysqli,"SELECT userID FROM user WHERE userID = '$username' AND passhash = '$password' AND isAdmin");
+$query = mysqli_query($mysqli,"SELECT userID FROM User WHERE userID = '$username' AND passhash = '$password' AND isAdmin");
 $total = mysqli_num_rows($query);
 
 if($total > 0)
