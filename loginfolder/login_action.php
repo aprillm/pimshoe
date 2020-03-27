ce6a1b8c1efacf065bc0a1f6d8abfdd9c96e453a
jckri <60411822+jckri@users.noreply.github.com>
2020-03-27 11:58:41 -0400

    Add files via upload

---
+++ loginfolder/login_action.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "pwdpwd";
+$dbname = "pimshoe";
+
+
+// Create connection
+$mysqli = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($mysqli->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['uname'];
+$password = sha1($_POST['psw']);
+
+
+$query = mysqli_query($mysqli,"SELECT userID FROM user WHERE userID = '$username' AND passhash = '$password' AND isAdmin");
+$total = mysqli_num_rows($query);
+
+
+if($total > 0)
+{
+  header('Location: AdminLanding.html');
+}
+else {
+  header('Location: badlogin.php');
+}
+
+?>
