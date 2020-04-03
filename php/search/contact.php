faf19988014c7234ce1ab866898b7c6ec7fac0f1
tylerhiu <60403530+tylerhiu@users.noreply.github.com>
2020-04-02 21:31:15 -0400

    Create contact.php

---
+++ php/search/contact.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  </head>
+ 
+
+  <body>
+    <div class="container-fluid jumbotron text-center bg-danger text-white" style="margin-bottom:0">
+      <h1>PIMSHOE</h1>
+    </div>
+
+    <div class="container text-center mt-5" style="margin-bottom:0">
+     
+    </div>
+	<section class="mb-4">
+	<div class="mx-auto" style="width: 600px;">
+    <!--Section heading-->
+    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+    <!--Section description-->
+    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+        a matter of hours to help you.</p>
+
+    <div class="row">
+
+        <!--Grid column-->
+        <div class="col-md-9 mb-md-0 mb-5">
+        
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+            <?php
+				if(isset( $_POST['name']))
+				$name = $_POST['name'];
+				if(isset( $_POST['email']))
+				$email = $_POST['email'];
+				if(isset( $_POST['message']))
+				$message = $_POST['message'];
+				if(isset( $_POST['subject']))
+				$subject = $_POST['subject'];
+
+				$content="From: $name \n Email: $email \n Message: $message";
+				$recipient = "PIMSHOE@PIMSHOE.com";
+				$mailheader = "From: $email \r\n";
+				mail($recipient, $subject, $content, $mailheader) or die("Error!");
+				echo "Email sent!";
+				?>
+
+              
+</div>
+ 
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
