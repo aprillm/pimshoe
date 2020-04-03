46fda4b230c1ab6cc4eeff3f40a9bd16e01b5c25
tylerhiu <60403530+tylerhiu@users.noreply.github.com>
2020-04-03 11:58:30 -0400

    Update contact.php

--- php/search/contact.php
+++ php/search/contact.php
@@ -19,7 +19,7 @@
      
     </div>
 	<section class="mb-4">
-	<div class="mx-auto" style="width: 600px;">
+	<div class="mx-auto" style="width: 550px;">
     <!--Section heading-->
     <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
     <!--Section description-->
@@ -33,24 +33,109 @@
         
             <form id="contact-form" name="contact-form" action="mail.php" method="POST">
 <?php
-	if(isset( $_POST['name']))
-	$name = $_POST['name'];
-	if(isset( $_POST['email']))
-	$email = $_POST['email'];
-	if(isset( $_POST['message']))
-	$message = $_POST['message'];
-	if(isset( $_POST['subject']))
-	$subject = $_POST['subject'];
+if(isset( $_POST['name']))
+$name = $_POST['name'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+if(isset( $_POST['message']))
+$message = $_POST['message'];
+if(isset( $_POST['subject']))
+$subject = $_POST['subject'];
 
-	$content="From: $name \n Email: $email \n Message: $message";
-	$recipient = "PIMSHOE@PIMSHOE.com";
-	$mailheader = "From: $email \r\n";
-	mail($recipient, $subject, $content, $mailheader) or die("Error!");
-	echo "Email sent!";
+$content="From: $name \n Email: $email \n Message: $message";
+$recipient = "PIMSHOE@PIMSHOE.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+echo "Email sent!";
 ?>
 
-              
+                <!--Grid row-->
+                <div class="row">
 
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="name" name="name" class="form-control">
+                            <label for="name" class="">Your name</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="email" name="email" class="form-control">
+                            <label for="email" class="">Your email</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="md-form mb-0">
+                            <input type="text" id="subject" name="subject" class="form-control">
+                            <label for="subject" class="">Subject</label>
+                        </div>
+                    </div>
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-12">
+
+                        <div class="md-form">
+                            <textarea type="text" id="message" name="message" rows="8" class="form-control md-textarea"></textarea>
+                            <label for="message">Your message</label>
+                        </div>
+
+                    </div>
+                </div>
+                <!--Grid row-->
+
+            </form>
+
+            <div class="text-right text-md-right">
+                <a class="btn btn-danger text-white" onclick="document.getElementById('contact-form').submit();">Send</a>
+                 <a href="home.php" class="btn btn-danger">
+      Back to home
+    </a>
+            </div>
+          
+   
+  
+            <div class="status"></div>
+        </div>
+        
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-md-3 text-center">
+            <ul class="list-unstyled mb-0">
+                <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                    <p>Newport News, VA 23606, USA</p>
+                </li>
+
+                <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                    <p>111-111-1111</p>
+                </li>
+
+                <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                    <p>PIMSHOE@PIMSHOE.com</p>
+                </li>
+            </ul>
+        </div>
+        <!--Grid column-->
+
+    </div>
+
+</section>
 <!--Section: Contact v.2-->
 </div>
  
