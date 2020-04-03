1d30a588ce63480d5f754f51a6a7cab2bb5a09e9
tylerhiu <60403530+tylerhiu@users.noreply.github.com>
2020-04-03 12:04:26 -0400

    Create about.php

---
+++ php/search/about.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  </head>
+ 
+
+  <body>
+    <div class="container-fluid jumbotron text-center bg-danger text-white" style="margin-bottom:0">
+      <h1>PIMSHOE</h1>
+    </div>
+
+    <div class="container text-center mt-5" style="margin-bottom:0">
+     
+    </div>
+	<section class="mb-4">
+	<div class="mx-auto" style="width: 550px;">
+    <!--Section heading-->
+    <h2 class="h1-responsive font-weight-bold text-center my-4">About Us</h2>
+    <!--Section description-->
+    <p class="text-center w-responsive mx-auto mb-5">We're a shoe store based in Newport News. Our founders Tyler Hiu, Joe Kriviski and April Morrison
+    are committed to helping you find the best fit for your feet! Our extensive inventory of shoe products and shoe accessories 
+    has something for you! Not sure about what you want? Our experienced and thoughtful staff will help you choose the product
+    that is right for you!</p>
+
+    
+
+            <div class="text-right text-md-center">
+                
+                 <a href="home.php" class="btn btn-danger">
+      			Back to home
+    			</a>
+            </div>
+          
+   
+  
+         
+    </div>
+
+	</section>
+
+	</div>
+ 
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
