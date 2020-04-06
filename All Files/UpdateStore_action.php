cb38d6d7b94df26131d689df89c5828b6ab1ca51
jckri <60411822+jckri@users.noreply.github.com>
2020-04-06 10:51:28 -0400

    Update UpdateStore_action.php

--- All Files/UpdateStore_action.php
+++ All Files/UpdateStore_action.php
@@ -10,10 +10,10 @@
   <body>
 <?php
 session_start();
-$servername = "localhost";
-$username = "root";
-$password = "pwdpwd";
-$dbname = "pimshoe";
+$servername = "cpsc445-capstone.cah4eqmlcf2h.us-east-1.rds.amazonaws.com";
+$username = "administrator";
+$password = "youdiedpimshoe";
+$dbname = "PIMSHOE";
 
 
 // Create connection
@@ -31,7 +31,7 @@ $State = $_POST['editstate'];
 $Zip = $_POST['editzip'];
 $Store = $_POST['editname'];
 
-$sql = "UPDATE store SET storeName='$Store', telephone='$Phone', streetAddress='$Street', city='$Zip', state='$State', zip='$Zip' WHERE storeID='$StoreID'";
+$sql = "UPDATE Store SET storeName='$Store', telephone='$Phone', streetAddress='$Street', city='$Zip', state='$State', zip='$Zip' WHERE storeID='$StoreID'";
 
 if (mysqli_query($conn, $sql)) {
     ?>
