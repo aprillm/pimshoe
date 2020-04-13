360f533e04d35dc4c3a2b0e1b24173b78e0b8f85
jckri <60411822+jckri@users.noreply.github.com>
2020-04-13 14:59:31 -0400

    Update AdminLanding.php

--- All Files/AdminLanding.php
+++ All Files/AdminLanding.php
@@ -58,7 +58,7 @@ include_once 'dbconn.php';
         </a>
         </div>
         <div class="col-sm-4">
-            <a href="#" class="btn btn-primary btn-block btn-lg p-5">
+          <a href="AdminManageDiscounts.html" class="btn btn-primary btn-block btn-lg p-5">
           Discount Manager
           </a>
           </div>
