f899529653072d543a8444fc3a8ed2e50c55bb7a
jckri <60411822+jckri@users.noreply.github.com>
2020-04-13 17:33:22 -0400

    Update login.php

--- All Files/login.php
+++ All Files/login.php
@@ -1,5 +1,18 @@
 <?php
-  include_once 'dbconn.php';
+  session_start();
+$servername = "cpsc445-capstone.cah4eqmlcf2h.us-east-1.rds.amazonaws.com";
+$username = "administrator";
+$password = "youdiedpimshoe";
+$dbname = "PIMSHOE";
+
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$dbname);
+
+// Check connection
+if (!$conn) {
+die("Connection failed: " . $mysqli_connect_error());
+}
 ?>
 <!DOCTYPE html>
 <html>
