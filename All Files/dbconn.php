b4abb3474bdf58d60b21adbb33852207511f63c5
jckri <60411822+jckri@users.noreply.github.com>
2020-04-13 17:38:21 -0400

    Update dbconn.php

--- All Files/dbconn.php
+++ All Files/dbconn.php
@@ -14,10 +14,4 @@ if (!$conn) {
 die("Connection failed: " . $mysqli_connect_error());
 }
 
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-
-} else {
-    echo "Please log in first to see this page.";
-    die();
-}
 ?>
